bef7f96b523187e8992521159ce693e152c5f929
None <yamldeveloper@proton.me>
2024-02-15 22:03:46 +0300

    port name as argument

--- tls_scan.py
+++ tls_scan.py
@@ -28,7 +28,7 @@ MAGENTA = "\x1b[35m"
 CYAN = "\x1b[36m"
 WHITE = "\x1b[37m"
 
-SERVICE_NAMES = {
+PORT_NAMES = {
     # Базовые
     465: "smtp",
     587: "smtp",
@@ -36,6 +36,7 @@ SERVICE_NAMES = {
     995: "pop",
     # vmware и многий другой софт висит на 443 и 8443 портах
     443: "https",
+    # доп-ый должен идти после основного
     8443: "https",
     # Далее пошли различные интересные приложения
     # удаленный доступ к рабочему столу
@@ -52,6 +53,9 @@ SERVICE_NAMES = {
     # https://subscription.packtpub.com/book/cloud-and-networking/9781849515849/1/ch01lvl1sec12/connecting-to-webmin#:~:text=Webmin%20uses%20port%2010000%20by,in%20on%20any%20network%20interface.
     10000: "webmin",
 }
+
+NAME_PORTS = {v: k for k, v in reversed(PORT_NAMES.items())}
+assert NAME_PORTS["https"] == 443
 
 
 BANNER = rf"""{GREEN}
@@ -100,7 +104,7 @@ def port_type(x: str) -> int | range:
         first, last = map(int, x.split("-"))
         return range(first, last)
     except ValueError:
-        return int(x)
+        return int(NAME_PORTS.get(x, x))
 
 
 def flatten(iterable: typing.Iterable) -> typing.Iterable:
@@ -153,15 +157,15 @@ def check_tls(
         return
     # logging.info(cert_dict)
     # {'subject': ((('organizationalUnitName', 'PVE Cluster Node'),), (('organizationName', 'Proxmox Virtual Environment'),), (('commonName', 'Pascal'),)), 'issuer': ((('commonName', 'Proxmox Virtual Environment'),), (('organizationalUnitName', '5c02d8c6-7c8d-4b2e-a4b5-8f06c0e380fd'),), (('organizationName', 'PVE Cluster Manager CA'),)), 'version': 3, 'serialNumber': '02', 'notBefore': 'Jan 23 15:26:13 2024 GMT', 'notAfter': 'Jan 22 15:26:13 2026 GMT', 'subjectAltName': (('IP Address', '127.0.0.1'), ('IP Address', '0:0:0:0:0:0:0:1'), ('DNS', 'localhost'), ('IP Address', '31.131.251.85'), ('DNS', 'Pascal'))}
-    service_name = SERVICE_NAMES.get(port, "unknown")
-    logging.info("found tls/ssl cert: %s:%d (%s)", ip, port, service_name)
+    port_name = PORT_NAMES.get(port, "unknown")
+    logging.info("found tls/ssl cert: %s:%d (%s)", ip, port, port_name)
     # for key in set(cert) & {"issuer", "subject"}:
     #     cert_dict[key] = dict(x[0] for x in cert_dict[key])
 
     res = {
         "ip": ip,
         "port": port,
-        "service_name": service_name,
+        "port_name": port_name,
         "cert": {
             # extensions?
             k: dict(x[0] for x in v) if k in ["issuer", "subject"] else v
@@ -227,8 +231,8 @@ def parse_args(
         dest="ports",
         nargs="*",
         type=port_type,
-        default=list(sorted(SERVICE_NAMES)),
-        help="port or FIRST_PORT-LAST_PORT",
+        default=list(sorted(PORT_NAMES)),
+        help="port, FIRST_PORT-LAST_PORT or port name (e.g., https, smtp, proxmox)",
     )
     parser.add_argument(
         "-i",
